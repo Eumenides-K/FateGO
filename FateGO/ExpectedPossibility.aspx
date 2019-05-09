9cdbd4d6f34b3ae84df3f46b73adc862a3a24a41
亦唐 李 <asd19991220@gamil.com>
2019-05-09 16:25:17 +0800

    一些简易的fgo工具

---
+++ FateGO/ExpectedPossibility.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ExpectedPossibility.aspx.cs" Inherits="FateGO.ExpectedPossibility" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            输入抽卡次数<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="预测概率" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ FateGO/ExpectedPossibility.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FateGO
+{
+    public partial class ExpectedPossibility : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(TextBox1.Text);
+                double ssr = (1 - Math.Pow(0.993, a)) * 100;
+                double sr = (1 - Math.Pow(0.985, a)) * 100;
+                Response.Write("抽到当期单up五星的概率为" + ssr + "%</br>抽到当期单up四星的概率为" + sr + "%");
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex);
+            }
+        }
+    }
+}
